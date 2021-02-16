99dbc4ce1edda0f6bc0f6e0eb055dacc3fcd8a28
Alexander Trakhimenok <a_trakhimenok@dellteam.com>
2021-02-16 12:47:02 +0000

    initial nx workspaces for both pnpm & yarn package managers with only 'emptyapp' - compiles & runs OK

---
+++ sneat-yarn/apps/emptyapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sneat-yarn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'emptyapp';
+}
