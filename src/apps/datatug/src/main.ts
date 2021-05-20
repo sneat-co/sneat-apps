c44dfce66e9bcc604db73554316f70e5b8fd3c31
Alexander Trakhimenok <alex@trakhimenok.com>
2021-05-20 08:25:23 +0100

    main.ts - comment change to test build time

--- src/apps/datatug/src/main.ts
+++ src/apps/datatug/src/main.ts
@@ -1,4 +1,4 @@
-// (C) https://datatug.app
+// 2021 (C) https://datatug.app
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
