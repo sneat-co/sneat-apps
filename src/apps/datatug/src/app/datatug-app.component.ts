444e59cdb799d778991b900228effdde14464543
Alexander Trakhimenok <alex@trakhimenok.com>
2021-06-01 16:51:25 +0100

    TODO: for some reason was failing when changed to datatug-root

--- src/apps/datatug/src/app/datatug-app.component.ts
+++ src/apps/datatug/src/app/datatug-app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {AppComponentService} from '@sneat/app';
 
 @Component({
-  selector: 'sneat-root',
+  selector: 'sneat-root', // TODO: for some reason was failing when changed to datatug-root
   templateUrl: 'datatug-app.component.html',
 })
 export class DatatugAppComponent {
