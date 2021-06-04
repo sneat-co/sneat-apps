b0e9198708448271aaf2ad011611aa44c8c15070
Alexander Trakhimenok <alex@trakhimenok.com>
2021-06-04 22:00:08 +0100

    obsolete TODO removed

--- src/apps/datatug/src/app/datatug-app.component.ts
+++ src/apps/datatug/src/app/datatug-app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {AppComponentService} from '@sneat/app';
 
 @Component({
-	selector: 'datatug-root', // TODO: for some reason was failing when changed to datatug-root
+	selector: 'datatug-root',
 	templateUrl: 'datatug-app.component.html',
 })
 export class DatatugAppComponent {
