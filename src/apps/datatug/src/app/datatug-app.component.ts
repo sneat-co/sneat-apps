c385e5ee2acd82be7bf475ba7337739efa359e37
Alexander Trakhimenok <alex@trakhimenok.com>
2021-06-04 21:59:52 +0100

    padding formatting

--- src/apps/datatug/src/app/datatug-app.component.ts
+++ src/apps/datatug/src/app/datatug-app.component.ts
@@ -2,13 +2,13 @@ import {Component} from '@angular/core';
 import {AppComponentService} from '@sneat/app';
 
 @Component({
-  selector: 'datatug-root', // TODO: for some reason was failing when changed to datatug-root
-  templateUrl: 'datatug-app.component.html',
+	selector: 'datatug-root', // TODO: for some reason was failing when changed to datatug-root
+	templateUrl: 'datatug-app.component.html',
 })
 export class DatatugAppComponent {
-  constructor(
-    readonly appComponentService: AppComponentService,
-  ) {
-    appComponentService.initializeApp();
-  }
+	constructor(
+		readonly appComponentService: AppComponentService,
+	) {
+		appComponentService.initializeApp();
+	}
 }
