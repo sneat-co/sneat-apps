16d51ecab7af63a80d19f1c62d5b87f2786f7d3c
Alexander Trakhimenok <alex@trakhimenok.com>
2021-06-21 18:37:15 +0100

    new project ui tweaks

--- src/libs/datatug/project/src/lib/new-project/new-project-form.component.ts
+++ src/libs/datatug/project/src/lib/new-project/new-project-form.component.ts
@@ -1,5 +1,5 @@
-import {Component, Inject, Input} from "@angular/core";
-import {PopoverController} from "@ionic/angular";
+import {Component, Inject, Input, ViewChild} from "@angular/core";
+import {IonInput, PopoverController, ViewDidEnter} from "@ionic/angular";
 import {ErrorLogger, IErrorLogger} from "@sneat/logging";
 import {ProjectService} from "@sneat/datatug/services/project";
 import {IProjStoreRef} from "@sneat/datatug/models";
@@ -9,11 +9,15 @@ import {DatatugNavService} from "@sneat/datatug/services/nav";
 	selector: 'datatug-new-project-form',
 	templateUrl: 'new-project-form.component.html'
 })
-export class NewProjectFormComponent {
+export class NewProjectFormComponent implements ViewDidEnter {
 	store = 'cloud';
 	title = '';
 
+	isCreating = false;
+
 	@Input() onCancel?: () => void;
+
+	@ViewChild(IonInput, {static: false}) titleInput?: IonInput;
 
 	constructor(
 		@Inject(ErrorLogger) private readonly errorLogger: IErrorLogger,
@@ -23,6 +27,12 @@ export class NewProjectFormComponent {
 	) {
 	}
 
+	ionViewDidEnter(): void {
+        setTimeout(() => {
+        	this.titleInput?.setFocus();
+		}, 100);
+    }
+
 	cancel(): void {
 		if (this.onCancel) {
 			this.onCancel();
@@ -31,6 +41,7 @@ export class NewProjectFormComponent {
 
 	create(): void {
 		console.log('NewProjectFormComponent.create()');
+		this.isCreating = true;
 		const projStoreRef: IProjStoreRef = {
 			type: 'firestore',
 		};
@@ -41,7 +52,10 @@ export class NewProjectFormComponent {
 				this.popoverController.dismiss().catch(this.errorLogger.logErrorHandler('failed to close popover with new project form'));
 				this.nav.goProject({storeId: projStoreRef.type, projectId});
 			},
-			error: this.errorLogger.logErrorHandler('Failed to create a new project'),
+			error: err => {
+				this.errorLogger.logError(err,'Failed to create a new project');
+				this.isCreating = false;
+			},
 		});
 	}
 }
