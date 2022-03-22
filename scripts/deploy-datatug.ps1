cebb6649d1099e104fb2b98384bc2fc6a545d5d3
Alexander Trakhimenok <alex@trakhimenok.com>
2022-03-22 11:49:07 +0000

    deploy-datatug.ps1 - works correctly from /scripts folder

--- scripts/deploy-datatug.ps1
+++ scripts/deploy-datatug.ps1
@@ -1,8 +1,11 @@
+$location = Get-Location
 Set-Location -Path $PSScriptRoot/../src
-pnpm run nx build datatug -- --prod
-Set-Location -Path $PSScriptRoot/
+#pnpm run nx build datatug -- --prod
+rm -rf $PSScriptRoot/../websites/datatug/pwa
 copy-item $PSScriptRoot/../src/dist/apps/datatug $PSScriptRoot/../websites/datatug/pwa -force -recurse
-$pwaIndexHtml = '../websites/datatug/pwa/index.html'
-(Get-Content $pwaIndexHtml) -replace '<base href="/"/>', '<base href="/pwa/"/>' | Out-File -encoding UTF8 $pwaIndexHtml
+Set-Location -Path $PSScriptRoot/..
+$pwaIndexHtml = 'websites/datatug/pwa/index.html'
+(Get-Content $pwaIndexHtml) -replace '<base href="/"', '<base href="/pwa/"' | Out-File -encoding UTF8 $pwaIndexHtml
 Set-Location -Path $PSScriptRoot/../websites
 firebase deploy --only hosting:datatug
+Set-Location -Path $location
