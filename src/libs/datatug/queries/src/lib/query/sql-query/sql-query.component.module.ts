5d23ffc6f3acfcf807314aab4747a74c9b10329d
Alexander Trakhimenok <alex@trakhimenok.com>
2022-03-22 10:55:01 +0000

    cleanup WIP datatug-qeuries is building

--- src/libs/datatug/queries/src/lib/query/sql-query/sql-query.component.module.ts
+++ src/libs/datatug/queries/src/lib/query/sql-query/sql-query.component.module.ts
@@ -1,44 +1,39 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-
 import { IonicModule } from "@ionic/angular";
-
-import { SqlQueryEditorComponent } from "./sql-query-editor.component";
 import { CodemirrorModule } from "@ctrl/ngx-codemirror";
 import { DatatugComponentsDatagridModule } from "@sneat/datatug/components/datagrid";
 import { DatatugExecutorModule } from "@sneat/datatug/executor";
 import { DatatugComponentsSqlEditorModule } from "@sneat/datatug/components/sqleditor";
 import { DatatugQueriesServicesModule } from "../../datatug-queries-services.module";
-import { ColumnsComponent } from "./query-builder/columns.component";
-import { JoinsComponent } from "./query-builder/joins.component";
-import { WormholeModule } from "@sneat/wormhole";
+// import { WormholeModule } from "@sneat/wormhole";
 import { DatatugComponentsParametersModule } from "@sneat/datatug/components/parameters";
 import { DatatugBoardUiModule } from "@sneat/datatug/board/ui";
 
-const exports = [
-	SqlQueryEditorComponent
-];
+import { SqlQueryEditorComponent } from "./sql-query-editor.component";
+import { ColumnsComponent } from "./query-builder/columns.component";
+import { JoinsComponent } from "./query-builder/joins.component";
 
 @NgModule({
 	imports: [
 		CommonModule,
 		FormsModule,
 		IonicModule,
-		DatatugComponentsSqlEditorModule,
 		CodemirrorModule,
+		// WormholeModule,
+		DatatugComponentsSqlEditorModule,
 		DatatugQueriesServicesModule,
 		DatatugExecutorModule,
 		DatatugComponentsDatagridModule,
-		WormholeModule,
 		DatatugComponentsParametersModule,
 		DatatugBoardUiModule
 	],
 	exports: [
-		...exports
+		SqlQueryEditorComponent,
 	],
 	declarations: [
-		...exports,
+		SqlQueryEditorComponent,
 		ColumnsComponent,
 		JoinsComponent
 	]
