0b3f46613ec9584e0651b4d37f5e92d3c1c644f7
Alexander Trakhimenok <alex@trakhimenok.com>
2022-03-22 10:05:22 +0000

    /scripts folder

---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+# Scripts for working with Sneat apps
+
+# deploy.ps1
+
+Builds & deploys apps with respective website.
+To run pass app name as 1st parameter as:
+
+> deploy.ps1 datatug
--- scripts/deploy-datatug.ps1
+++ scripts/deploy-datatug.ps1
@@ -1,6 +1,7 @@
-
-Set-Location -Path $PSScriptRoot
-copy-item $PSScriptRoot/../src/dist/apps/datatug $PSScriptRoot/datatug/pwa -force -recurse
+Set-Location -Path $PSScriptRoot/../src
+pnpm run nx build datatug -- --prod
+Set-Location -Path $PSScriptRoot/
+copy-item $PSScriptRoot/../src/dist/apps/datatug $PSScriptRoot/../websites/datatug/pwa -force -recurse
 (Get-Content $PSScriptRoot/datatug/pwa/index.html) -replace '<base href="/"/>', '<base href="/pwa/"/>' | Out-File -encoding UTF8 $PSScriptRoot/datatug/pwa/index.html
 #firebase deploy --only hosting:datatug
 
