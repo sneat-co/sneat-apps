bf2e89d82498b4072e730021ef126b46a7eda529
Alexander Trakhimenok <alex@trakhimenok.com>
2022-04-19 10:28:10 +0100

    members page component

--- src/libs/team/components/src/lib/members-list/members-list.module.ts
+++ src/libs/team/components/src/lib/members-list/members-list.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { MembersListComponent } from '@sneat/team/components';
+import { MembersListComponent } from './members-list.component';
 
 @NgModule({
 	imports: [
