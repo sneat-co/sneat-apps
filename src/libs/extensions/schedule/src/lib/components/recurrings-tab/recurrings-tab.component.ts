4f778e56ae2a8358125dac345945f0d769d53827
Alexander Trakhimenok <alex@trakhimenok.com>
2022-04-29 11:56:22 +0100

    recurrings tab - filter stats & filter reset

--- src/libs/extensions/schedule/src/lib/components/recurrings-tab/recurrings-tab.component.ts
+++ src/libs/extensions/schedule/src/lib/components/recurrings-tab/recurrings-tab.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { IHappeningWithUiState } from '@sneat/dto';
 import { ITeamContext } from '@sneat/team/models';
 import { IScheduleFilter } from '../schedule-filter/schedule-filter';
@@ -14,5 +14,10 @@ export class RecurringsTabComponent {
 	@Input() allRecurrings?: IHappeningWithUiState[];
 	@Input() team?: ITeamContext;
 
+	@Output() resetFilter = new EventEmitter<Event>();
+
+	public get numberOfHidden(): number {
+		return (this.allRecurrings?.length || 0) - (this.recurrings?.length || 0)
+	}
 	readonly id = (i: number, v: { id: string }): string => v.id;
 }
