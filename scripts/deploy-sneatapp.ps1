a0cdb72990df5e262c3d117f20a97833822bef85
Alexander Trakhimenok <alex@trakhimenok.com>
2022-05-07 09:18:19 +0100

    deploy-sneatapp.ps1

--- scripts/deploy-sneatapp.ps1
+++ scripts/deploy-sneatapp.ps1
@@ -1,10 +1,10 @@
 $location = Get-Location
 Set-Location -Path $PSScriptRoot/../src
 pnpm run nx build sneat-app
-rm -rf $PSScriptRoot/../websites/sneatteam/pwa
-copy-item $PSScriptRoot/../src/dist/apps/sneat-app $PSScriptRoot/../websites/sneatteam/pwa -force -recurse
+rm -rf $PSScriptRoot/../websites/sneat.app/pwa
+copy-item $PSScriptRoot/../src/dist/apps/sneat-app $PSScriptRoot/../websites/sneat.app/pwa -force -recurse
 Set-Location -Path $PSScriptRoot/..
-$pwaIndexHtml = 'websites/sneatteam/pwa/index.html'
+$pwaIndexHtml = 'websites/sneat.app/pwa/index.html'
 (Get-Content $pwaIndexHtml) -replace '<base href="/"', '<base href="/pwa/"' | Out-File -encoding UTF8 $pwaIndexHtml
 Set-Location -Path $PSScriptRoot/../websites
 firebase deploy --only hosting:sneatapp
