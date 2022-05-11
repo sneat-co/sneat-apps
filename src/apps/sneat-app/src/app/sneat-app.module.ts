c5bb6ef4f19fa92589d702182ec68096af70f66f
Alexander Trakhimenok <alex@trakhimenok.com>
2022-05-11 09:15:03 +0100

    cleanup imports in SneatAppModule

--- src/apps/sneat-app/src/app/sneat-app.module.ts
+++ src/apps/sneat-app/src/app/sneat-app.module.ts
@@ -1,10 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Router, RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 import { DefaultSneatAppApiBaseUrl, SneatApiBaseUrl } from '@sneat/api';
 import { initFirebase, SneatApplicationModule } from '@sneat/app';
 import { AuthMenuItemModule, SneatAuthServicesModule } from '@sneat/auth';
@@ -16,8 +15,6 @@ import { environment } from '../environments/environment';
 import { SneatAppMenuComponent } from './sneat-app-menu-component/sneat-app-menu.component';
 import { SneatAppRoutingModule } from './sneat-app-routing.module';
 import { SneatAppComponent } from './sneat-app.component';
-import { TraceService, createErrorHandler } from '@sentry/angular';
-import { APP_INITIALIZER } from '@angular/core';
 
 initFirebase(environment.firebaseConfig);
 
