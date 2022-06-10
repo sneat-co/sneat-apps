8288e51768153e1aa2772f93990669997a5d3141
Alexander Trakhimenok <alex@trakhimenok.com>
2022-06-10 13:15:52 +0100

    fixed import of meeting in timer

--- src/libs/timer/src/lib/timer.module.ts
+++ src/libs/timer/src/lib/timer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { TimerFactory } from '../../../meeting/src/lib/timer/timer.service';
 import { SneatLoggingModule } from '@sneat/logging';
+import { TimerFactory } from '@sneat/meeting';
 import { RetroTimerComponent } from './components/retro-timer/retro-timer.component';
 import { TimerMeetingButtonComponent } from './components/timer/timer-meeting-button/timer-meeting-button.component';
 import { TimerMemberButtonComponent } from './components/timer/timer-member-button/timer-member-button.component';
