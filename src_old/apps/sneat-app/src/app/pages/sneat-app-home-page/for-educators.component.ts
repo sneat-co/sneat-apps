92c3b12cdf856a9712eb115dc1624f3ad8bcf1d9
Alexander Trakhimenok <alex@trakhimenok.com>
2022-06-18 10:53:27 +0100

    removed sneat-app from src_old

--- src_old/apps/sneat-app/src/app/pages/sneat-app-home-page/for-educators.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'sneat-for-educators',
-	templateUrl: './for-educators.component.html',
-})
-export class ForEducatorsComponent {
-
-}
