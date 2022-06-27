5b169c4c6bb614caa9d5ad9f002be9ce31876f31
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2022-06-27 05:44:32 +0100

    export ContactSelectorComponent

--- src/libs/extensions/contactus/src/lib/components/index.ts
+++ src/libs/extensions/contactus/src/lib/components/index.ts
@@ -1,2 +1,4 @@
 export * from './shared-with/shared-with.module';
 export * from './shared-with/shared-with.component';
+export * from './contact-selector/contact-selector.component';
+export * from './contact-selector/contact-selector.module';
