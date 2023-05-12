5f46a5e1009e7d6a7baa33125e3a829b8d65e650
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-05-12 13:44:35 +0100

    fix(sneat-app): compilation issues fixed

--- src/libs/core/src/lib/nav/nav-context.ts
+++ src/libs/core/src/lib/nav/nav-context.ts
@@ -4,6 +4,6 @@ export type DeleteOperationState = 'deleting' | 'deleted' | undefined;
 
 export interface INavContext<Brief extends {id: string}, Dto> {
 	readonly id: string;
-	readonly brief?: Brief;
+	readonly brief?: Brief | null;
 	readonly dto?: Dto | null;
 }
