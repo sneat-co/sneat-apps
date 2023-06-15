d67238f0ba0799af1f91240cc6b955e01fc8754d
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-06-15 07:31:22 +0100

    chore(scripts): serve.sh checks for existence of project argument

--- src/serve.sh
+++ src/serve.sh
@@ -1,1 +1,6 @@
-sudo pnpm run nx serve "$1" --disable-host-check --host local.host --port 80 #--optimization=false --sourceMap=true
+if [ -n "$1" ]
+then
+	sudo pnpm run nx serve "$1" --disable-host-check --host local.host --port 80 #--optimization=false --sourceMap=true
+else
+	printf "Please provide a project name as first argument\n"
+fi
