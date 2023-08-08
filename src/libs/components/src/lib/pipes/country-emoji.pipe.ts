5fca2dcd78cceea4b328b0ad46c3351262ddcbd4
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-08 14:53:55 +0100

    fix: countryFlagEmoji

--- src/libs/components/src/lib/pipes/country-emoji.pipe.ts
+++ src/libs/components/src/lib/pipes/country-emoji.pipe.ts
@@ -2,10 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { countriesByID } from '../country-selector';
 
 export function countryFlagEmoji(countryID?: string): string {
-	if (countryID) {
-		return countriesByID[countryID].emoji || countryID;
-	}
-	return '';
+	return countryID ? countriesByID[countryID]?.emoji || countryID : '';
 }
 
 @Pipe({ name: 'countryFlag' })
@@ -18,6 +15,6 @@ export class CountryFlagPipe implements PipeTransform {
 @Pipe({ name: 'countryTitle' })
 export class CountryTitle implements PipeTransform {
 	transform(countryID?: string): string {
-		return countryID ? countriesByID[countryID].title || countryID : '';
+		return countryID ? countriesByID[countryID]?.title || countryID : '';
 	}
 }
