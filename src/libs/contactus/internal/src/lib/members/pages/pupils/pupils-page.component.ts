760eaa7074a2b075be44e73061ea6161a69db411
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-10 14:53:44 +0100

    chore: cleanup old mess

--- src/libs/contactus/internal/src/lib/members/pages/pupils/pupils-page.component.ts
+++ src/libs/contactus/internal/src/lib/members/pages/pupils/pupils-page.component.ts
@@ -1,41 +1,38 @@
 import {Component} from '@angular/core';
-import {MembersBasePage} from '../../members-base-page';
-import {CommuneBasePageParams} from 'sneat-shared/services/params';
-import {IMemberService} from 'sneat-shared/services/interfaces';
-import {NgModulePreloaderService} from 'sneat-shared/services/ng-module-preloader.service';
-import {MemberType} from 'sneat-shared/models/types';
-import {ICommuneMemberInfo} from '../../../../models/dto/dto-commune';
+// import {MembersBasePage} from '../../members-base-page';
+// import {CommuneBasePageParams} from 'sneat-shared/services/params';
 
 @Component({
 	selector: 'sneat-pupils',
 	templateUrl: './pupils-page.component.html',
-	providers: [CommuneBasePageParams],
+	// providers: [CommuneBasePageParams],
 })
-export class PupilsPageComponent extends MembersBasePage {
+export class PupilsPageComponent /*extends MembersBasePage*/ {
 
 	segment: 'active' | 'archive' = 'active';
 
-	filteredMembers: ICommuneMemberInfo[] | undefined;
+	// filteredMembers: ICommuneMemberInfo[] | undefined;
 
 	applyFilter(filter: string): void {
-		if (filter) {
-			const v = filter.toLowerCase();
-			this.filteredMembers = this.members.filter(m => m.title && m.title.toLowerCase()
-				.indexOf(v) >= 0);
-		} else {
-			this.filteredMembers = undefined;
-		}
+		console.log('applyFilter', filter);
+		// if (filter) {
+		// 	const v = filter.toLowerCase();
+		// 	this.filteredMembers = this.members.filter(m => m.title && m.title.toLowerCase()
+		// 		.indexOf(v) >= 0);
+		// } else {
+		// 	this.filteredMembers = undefined;
+		// }
 	}
 
 	constructor(
-		params: CommuneBasePageParams,
-		membersService: IMemberService,
-		preloader: NgModulePreloaderService,
+		// params: CommuneBasePageParams,
+		// membersService: IMemberService,
+		// preloader: NgModulePreloaderService,
 	) {
-		super(params, membersService, preloader);
+		// super(params, membersService, preloader);
 	}
 
-	get memberType(): MemberType {
-		return 'pupil';
-	}
+	// get memberType(): MemberType {
+	// 	return 'pupil';
+	// }
 }
