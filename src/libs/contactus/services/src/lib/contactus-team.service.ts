d9f981a857d1a201f6126e4d3b47e3dfe8964b82
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-09-30 07:58:00 +0100

    fix(contactus): watchContactBriefs

--- src/libs/contactus/services/src/lib/contactus-team.service.ts
+++ src/libs/contactus/services/src/lib/contactus-team.service.ts
@@ -1,30 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Firestore as AngularFirestore } from '@angular/fire/firestore';
-import { IContactContext, IContactusTeamDto, ITeamContext } from '@sneat/team/models';
-import { TeamModuleService } from '@sneat/team/services';
-import { map, Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Firestore as AngularFirestore } from "@angular/fire/firestore";
+import { IContactBrief, IContactDto } from "@sneat/dto";
+import { IContactusTeamDto, ITeamContext } from "@sneat/team/models";
+import { TeamModuleService } from "@sneat/team/services";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class ContactusTeamService extends TeamModuleService<IContactusTeamDto, IContactusTeamDto> {
 
-	public constructor(
-		afs: AngularFirestore,
-		// sneatApiService: SneatApiService,
-	) {
-		super('contactus', afs);
+	public constructor(afs: AngularFirestore) {
+		super("contactus", afs);
 	}
 
-	watchContactBriefs(team: ITeamContext): Observable<IContactContext[]> {
-		const o = this.watchTeamModuleRecord(team);
-		const result = o.pipe(
-			map(contactusTeam => {
-				const contacts = contactusTeam?.dto?.contacts;
-				const cc: IContactContext[] = contacts
-					? Object.keys(contacts).map(id => ({ id, brief: contacts[id], team }))
-					: [];
-				return cc;
-			}),
-		);
-		return result;
-	}
+	readonly watchContactBriefs = (team: ITeamContext) => this.watchBriefs<IContactBrief, IContactDto>(team, dto => dto?.contacts || {});
 }
--- src/libs/team/services/src/lib/team-module.service.ts
+++ src/libs/team/services/src/lib/team-module.service.ts
@@ -1,9 +1,12 @@
-import { collection, CollectionReference, Firestore as AngularFirestore } from '@angular/fire/firestore';
-import { SneatApiService } from '@sneat/api';
-import { IDtoAndID, ITeamContext } from '@sneat/team/models';
-import { map } from 'rxjs/operators';
-import { TeamItemService } from './team-item.service';
-import { Observable, tap } from 'rxjs';
+import { collection, CollectionReference, Firestore as AngularFirestore } from "@angular/fire/firestore";
+import { SneatApiService } from "@sneat/api";
+import { IContactBrief } from "@sneat/dto";
+import { IDtoAndID, ITeamContext, ITeamItemContext } from "@sneat/team/models";
+import firebase from "firebase/compat";
+import { map } from "rxjs/operators";
+import { TeamItemService } from "./team-item.service";
+import { Observable, tap } from "rxjs";
+import Item = firebase.analytics.Item;
 
 export abstract class TeamModuleService<Brief, Dto extends Brief> extends TeamItemService<Brief, Dto> {
 
@@ -11,21 +14,39 @@ export abstract class TeamModuleService<Brief, Dto extends Brief> extends TeamIt
 
 	protected constructor(
 		moduleID: string,
-		afs: AngularFirestore,
+		afs: AngularFirestore
 	) {
 		// this.sfs = new SneatFirestoreService<Brief, Dto>(collectionName, afs);
-		super(moduleID, 'modules', afs, undefined as unknown as SneatApiService);
+		super(moduleID, "modules", afs, undefined as unknown as SneatApiService);
 	}
 
 	watchTeamModuleRecord(team: ITeamContext): Observable<IDtoAndID<Dto>> {
 		const logPrefix = `watchTeamModuleEntry(teamID=${team.id}, moduleID=${this.moduleID})`;
 		console.log(logPrefix);
-		const collectionRef = collection(this.teamsCollection, team.id, 'modules') as CollectionReference<Dto>;
+		const collectionRef = collection(this.teamsCollection, team.id, "modules") as CollectionReference<Dto>;
 		const result = this.sfs.watchByID<Dto>(collectionRef, this.moduleID)
 			.pipe(
 				map(o => ({ team, ...o })),
-				tap(o => console.log(`${logPrefix} =>`, o)),
+				tap(o => console.log(`${logPrefix} =>`, o))
 			);
+		return result;
+	}
+
+	watchBriefs<ItemBrief, ItemDto>(
+		team: ITeamContext,
+		getBriefs: (dto?: Dto) => Readonly<{
+			[id: string]: ItemBrief
+		}> ): Observable<ITeamItemContext<ItemBrief, ItemDto>[]> {
+		const o = this.watchTeamModuleRecord(team);
+		const result = o.pipe(
+			map(teamModule => {
+				const briefs = getBriefs(teamModule?.dto || undefined);
+				const items: ITeamItemContext<ItemBrief, ItemDto>[] = briefs
+					? Object.keys(briefs).map(id => ({ id, brief: briefs[id], team }))
+					: [];
+				return items;
+			})
+		);
 		return result;
 	}
 }
