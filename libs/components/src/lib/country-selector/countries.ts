4fc74e87a7bf06a9fff8d5d5b51d550f30c48a92
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-04-18 10:20:10 +0100

    feat: GeoRegion for countries

--- libs/components/src/lib/country-selector/countries.ts
+++ libs/components/src/lib/country-selector/countries.ts
@@ -1,26 +1,68 @@
+export type GeoRegion =
+	| 'Europe'
+	| 'Asia'
+	| 'South America'
+	| 'North America'
+	| 'Pacific Ocean'
+	| 'Africa';
+
 export interface ICountry {
 	id: string;
+	geoRegions: GeoRegion[];
 	title: string;
 	emoji: string;
 }
 
 export const countriesByID: Record<string, ICountry> = {
-	AU: { id: 'AU', title: 'Australia', emoji: '🇦🇺' },
-	EE: { id: 'EE', title: 'Estonia', emoji: '🇪🇪' },
-	LV: { id: 'LV', title: 'Latvia', emoji: '🇱🇻' },
-	LT: { id: 'LT', title: 'Lithuania', emoji: '🇱🇹' },
-	IE: { id: 'IE', title: 'Ireland', emoji: '🇮🇪' },
-	NZ: { id: 'NZ', title: 'New Zealand', emoji: '🇳🇿' },
-	RU: { id: 'RU', title: 'Russia', emoji: '🇷🇺' },
-	ES: { id: 'ES', title: 'Spain', emoji: '🇪🇸' },
-	UK: { id: 'UK', title: 'United Kingdom', emoji: '🇬🇧' },
-	UA: { id: 'UA', title: 'Ukraine', emoji: '🇺🇦' },
-	US: { id: 'US', title: 'United States Of America', emoji: '🇺🇸' },
+	AF: { id: 'AF', geoRegions: ['Asia'], title: 'Afghanistan', emoji: '🇦🇫' },
+	AU: {
+		id: 'AU',
+		geoRegions: ['Pacific Ocean'],
+		title: 'Australia',
+		emoji: '🇦🇺',
+	},
+	CA: {
+		id: 'CA',
+		geoRegions: ['North America'],
+		title: 'Canada',
+		emoji: '🇨🇦',
+	},
+	EE: { id: 'EE', geoRegions: ['Europe'], title: 'Estonia', emoji: '🇪🇪' },
+	LV: { id: 'LV', geoRegions: ['Europe'], title: 'Latvia', emoji: '🇱🇻' },
+	LT: { id: 'LT', geoRegions: ['Europe'], title: 'Lithuania', emoji: '🇱🇹' },
+	IE: { id: 'IE', geoRegions: ['Europe'], title: 'Ireland', emoji: '🇮🇪' },
+	NZ: {
+		id: 'NZ',
+		geoRegions: ['Pacific Ocean'],
+		title: 'New Zealand',
+		emoji: '🇳🇿',
+	},
+	RU: {
+		id: 'RU',
+		geoRegions: ['Europe', 'Asia'],
+		title: 'Russia',
+		emoji: '🇷🇺',
+	},
+	ES: { id: 'ES', geoRegions: ['Europe'], title: 'Spain', emoji: '🇪🇸' },
+	UK: {
+		id: 'UK',
+		geoRegions: ['Europe'],
+		title: 'United Kingdom',
+		emoji: '🇬🇧',
+	},
+	UA: { id: 'UA', geoRegions: ['Europe'], title: 'Ukraine', emoji: '🇺🇦' },
+	US: {
+		id: 'US',
+		geoRegions: ['North America'],
+		title: 'United States Of America',
+		emoji: '🇺🇸',
+	},
 };
 
 export const unknownCountry: ICountry = {
 	id: '--',
 	title: 'Unknown',
+	geoRegions: [],
 	emoji: '🏳️',
 };
 
