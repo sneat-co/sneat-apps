16234b1834fd8072d0e7e083719b54d9ce9cb6a9
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-07-11 01:55:55 +0100

    Revert "fix: use signal() in sign-in-from-email-link-page.component"
    This reverts commit bf55ac11799cbf2f353dcbaa95d777da73e62076.

--- libs/auth/ui/src/lib/pages/sign-in-from-email-link/sign-in-from-email-link-page.component.ts
+++ libs/auth/ui/src/lib/pages/sign-in-from-email-link/sign-in-from-email-link-page.component.ts
@@ -1,10 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-	ChangeDetectionStrategy,
-	Component,
-	Inject,
-	signal,
-} from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { IonicModule, NavController } from '@ionic/angular';
 import { SneatAuthStateService } from '@sneat/auth-core';
 import { ErrorLogger, IErrorLogger } from '@sneat/logging';
@@ -14,28 +9,27 @@ import { ErrorLogger, IErrorLogger } from '@sneat/logging';
 	templateUrl: 'sign-in-from-email-link-page.component.html',
 	standalone: true,
 	imports: [CommonModule, IonicModule],
-	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignInFromEmailLinkPageComponent {
-	protected readonly email = signal('');
-	protected readonly emailFromStorage = signal(false);
-	protected readonly isSigning = signal(false);
+	email: string;
+	emailFromStorage = false;
+	isSigning = false;
 
 	constructor(
 		@Inject(ErrorLogger) private readonly errorLogger: IErrorLogger,
 		private readonly authStateService: SneatAuthStateService,
 		private readonly navController: NavController,
 	) {
-		this.email.set(localStorage.getItem('emailForSignIn') || '');
-		this.emailFromStorage.set(!!this.email);
-		if (this.email()) {
+		this.email = localStorage.getItem('emailForSignIn') || '';
+		this.emailFromStorage = !!this.email;
+		if (this.email) {
 			this.signIn();
 		}
 	}
 
 	public signIn(): void {
-		this.isSigning.set(true);
-		this.authStateService.signInWithEmailLink(this.email()).subscribe({
+		this.isSigning = true;
+		this.authStateService.signInWithEmailLink(this.email).subscribe({
 			next: () => {
 				this.navController
 					.navigateRoot('/')
@@ -46,8 +40,8 @@ export class SignInFromEmailLinkPageComponent {
 					);
 			},
 			error: (err) => {
-				this.isSigning.set(false);
-				this.emailFromStorage.set(false);
+				this.isSigning = false;
+				this.emailFromStorage = false;
 				this.errorLogger.logError(err, 'Failed to sign in with email link');
 			},
 		});
