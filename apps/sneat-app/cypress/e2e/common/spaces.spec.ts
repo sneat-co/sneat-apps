5e70e80012120e594a6c283fe3b12676228e4e32
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-07-21 10:49:05 +0100

    fix(refactoring): renaming team=>space - CY fix

--- apps/sneat-app/cypress/e2e/common/spaces.spec.ts
+++ apps/sneat-app/cypress/e2e/common/spaces.spec.ts
@@ -7,11 +7,11 @@ export const assertSpacesDropdownIsVisible = () =>
 	getSpacesDropdown().should('be.visible');
 export const clickNewSpaceButton = () => getNewSpaceButton().click();
 export const getNewSpaceButton = () =>
-	cy.get('sneat-teams-menu ion-item ion-label:contains("Family ")');
+	cy.get('sneat-spaces-menu ion-item ion-label:contains("Family ")');
 
 export const getSpacesDropdown = () => {
 	return cy
-		.get('sneat-team-menu ion-list ion-item ion-select')
+		.get('sneat-space-menu ion-list ion-item ion-select')
 		.shadow()
 		.find('.label-text:contains(Space)')
 		.should('exist');
