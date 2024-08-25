80c137d1c93b8485f5487bd5bffa52674d547169
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-08-25 07:24:33 +0100

    fix: telegram mini app sign in WIP1

--- apps/sneat-app/src/app/sneat-app.component.ts
+++ apps/sneat-app/src/app/sneat-app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { TopMenuService } from '@sneat/core';
 
+declare const onTelegramScripLoaded: (() => void)[];
+
 @Component({
 	selector: 'sneat-app-root',
 	templateUrl: 'sneat-app.component.html',
 })
 export class SneatAppComponent {
-	constructor(protected readonly topMenuService: TopMenuService) {}
+	constructor(protected readonly topMenuService: TopMenuService) {
+		onTelegramScripLoaded.push(() => {
+			(window as unknown as any).Telegram.WebApp.ready();
+			// alert('SneatAppComponent notified that Telegram script has been loaded');
+		});
+	}
 }
