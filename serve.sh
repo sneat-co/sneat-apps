318c0f23f3d5d91112db55cf6630ec4f73f3b109
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-10-09 15:59:27 +0100

    fix(scripts): serve.sh

--- serve.sh
+++ serve.sh
@@ -1,6 +1,6 @@
 if [ -n "$1" ]
 then
-	sudo pnpm run nx serve "$1" --disable-host-check --host local.host --port 80 #--optimization=false --sourceMap=true
+	pnpm run nx serve "$1" --host local-app.sneat.ws #--disable-host-check --host local-app.sneat.ws --port 80 --optimization=false --sourceMap=true
 else
 	printf "Please provide a project name as first argument\n"
 fi
