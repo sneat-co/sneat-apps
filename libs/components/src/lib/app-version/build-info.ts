75ea105e4f70ddc6337b6933f1028d079b6399af
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-10-10 16:04:21 +0100

    fix: show 7 chars of short git version in build info

--- libs/components/src/lib/app-version/build-info.ts
+++ libs/components/src/lib/app-version/build-info.ts
@@ -2,6 +2,6 @@ export const buildInfo: {
 	readonly gitHash: string;
 	readonly buildTimestamp: string;
 } = {
-	gitHash: 'git hash t0be$et',
+	gitHash: 'gitHash t0be$et',
 	buildTimestamp: 'timestamp t0be$et',
 };
