21a16450a5df4bf1de5d672225ad383053611b2b
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2025-01-15 08:09:07 +0000

    fix: calendar-brief-card-component exported

--- libs/extensions/schedulus/main/src/lib/index.ts
+++ libs/extensions/schedulus/main/src/lib/index.ts
@@ -1,4 +1,5 @@
 export * from './schedulus-routing';
+export * from './components/calendar-brief-card/calendar-brief-card.component';
 export * from './components/calendar/calendar.component';
 export * from './services';
 export * from './components/calendar/weekday';
