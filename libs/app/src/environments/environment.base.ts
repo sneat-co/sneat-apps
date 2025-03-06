ccc7afd1770909b9cd61b7177949ff0668c5eaf8
Alexander Trakhimenok <alex@trakhimenok.com>
2025-03-06 06:49:37 +0000

    fix(deps): try/catch on signInWith

--- libs/app/src/environments/environment.base.ts
+++ libs/app/src/environments/environment.base.ts
@@ -1,4 +1,8 @@
-import { IEnvironmentConfig, IFirebaseConfig } from '../lib/environment-config';
+import {
+	IEnvironmentConfig,
+	IFirebaseConfig,
+	IFirebaseEmulatorConfig,
+} from '../lib/environment-config';
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -16,22 +20,24 @@ const useSSL = useNgrok || window.location.hostname == 'local-app.sneat.ws';
 // const nonSecureEmulatorHost = '127.0.0.1'; // 'localhost';
 const nonSecureEmulatorHost = '127.0.0.1'; // 'localhost';
 
+export const firebaseEmulatorConfig: IFirebaseEmulatorConfig = {
+	authPort: useSSL ? 443 : 9099,
+	authHost: useNgrok
+		? window.location.hostname
+		: useSSL
+			? 'local-fb-auth.sneat.ws'
+			: nonSecureEmulatorHost,
+	firestorePort: useSSL ? 443 : 8080,
+	firestoreHost: useNgrok
+		? window.location.hostname
+		: useSSL
+			? 'local-firestore.sneat.ws'
+			: nonSecureEmulatorHost,
+};
+
 const firebaseConfig: IFirebaseConfig = {
 	useEmulators,
-	emulator: {
-		authPort: useSSL ? 443 : 9099,
-		authHost: useNgrok
-			? window.location.hostname
-			: useSSL
-				? 'local-fb-auth.sneat.ws'
-				: nonSecureEmulatorHost,
-		firestorePort: useSSL ? 443 : 8080,
-		firestoreHost: useNgrok
-			? window.location.hostname
-			: useSSL
-				? 'local-firestore.sneat.ws'
-				: nonSecureEmulatorHost,
-	},
+	emulator: firebaseEmulatorConfig,
 	apiKey: 'AIzaSyAYGGhSQQ8gUcyPUcUOFW7tTSYduRD3cuw',
 	authDomain: 'sneat.app',
 	projectId: 'local-sneat-app', // The 'demo-' prefix is added if useEmulators is true
