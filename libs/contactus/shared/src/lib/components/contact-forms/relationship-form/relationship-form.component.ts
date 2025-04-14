d21672d0f536ac01ffd6937f7aafd5da650f45f8
Alexander Trakhimenok <alex@trakhimenok.com>
2025-04-14 15:32:57 +0100

    fix: relationship-form.component improvements

--- libs/contactus/shared/src/lib/components/contact-forms/relationship-form/relationship-form.component.ts
+++ libs/contactus/shared/src/lib/components/contact-forms/relationship-form/relationship-form.component.ts
@@ -1,6 +1,7 @@
 import {
 	ChangeDetectionStrategy,
 	Component,
+	computed,
 	EventEmitter,
 	input,
 	Input,
@@ -40,9 +41,10 @@ import {
 	ITitledRecord,
 	IWithCreatedShort,
 } from '@sneat/dto';
+import { ISelectItem, SelectFromListComponent } from '@sneat/ui';
 import { SpaceRelatedFormComponent } from '../space-related-form.component';
 
-const getRelOptions = (r: FamilyMemberRelation[]): ITitledRecord[] => [
+const getRelOptions = (r: FamilyMemberRelation[]): ISelectItem[] => [
 	...r.map((id) => ({ id, title: relationshipTitle(id) })),
 	{ id: MemberRelationshipOther, title: 'Other' },
 	{ id: MemberRelationshipUndisclosed, title: 'Undisclosed' },
@@ -59,15 +61,10 @@ interface IRelationshipWithID extends IRelationshipRole {
 		ReactiveFormsModule,
 		IonCard,
 		IonItem,
-		IonIcon,
-		IonSelect,
-		IonSelectOption,
-		IonItemGroup,
 		IonItemDivider,
 		IonLabel,
-		IonRadioGroup,
-		IonRadio,
 		IonBadge,
+		SelectFromListComponent,
 	],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	selector: 'sneat-relationship-form',
@@ -103,14 +100,37 @@ export class RelationshipFormComponent
 	// Defined here as it is used in the template twice
 	protected readonly label = 'Related to me as';
 
-	protected relationshipOptions?: ITitledRecord[];
+	protected $relationshipOptions = computed(() => {
+		switch (this.$spaceType()) {
+			case 'family': {
+				return getRelOptions(
+					this.$ageGroup() === 'child'
+						? ([
+								FamilyMemberRelation.child,
+								FamilyMemberRelation.sibling,
+								FamilyMemberRelation.cousin,
+							] as FamilyMemberRelation[])
+						: ([
+								FamilyMemberRelation.spouse,
+								FamilyMemberRelation.partner,
+								FamilyMemberRelation.child,
+								FamilyMemberRelation.sibling,
+								FamilyMemberRelation.cousin,
+								FamilyMemberRelation.parent,
+								FamilyMemberRelation.parentInLaw,
+								FamilyMemberRelation.grandparent,
+								FamilyMemberRelation.grandparentInLaw,
+							] as FamilyMemberRelation[]),
+				);
+			}
+			default:
+				return [];
+		}
+	});
 
 	override ngOnChanges(changes: SimpleChanges): void {
 		console.log('RelationshipFormComponent.ngOnChanges()', changes);
-		if (changes['ageGroup'] || changes['space']) {
-			this.setRelationships();
-		}
-		if (changes['relatedTo'] || changes['relatedItems']) {
+		if (changes['$relatedTo'] || changes['$relatedItems']) {
 			this.onRelatedChanged();
 		}
 	}
@@ -160,50 +180,7 @@ export class RelationshipFormComponent
 		}
 	}
 
-	//
-	// override onSpaceTypeChanged(): void {
-	//
-	//
-	// }
-
-	private setRelationships(): void {
-		switch (this.$spaceType()) {
-			case 'family': {
-				this.relationshipOptions = getRelOptions(
-					this.$ageGroup() === 'child'
-						? ([
-								FamilyMemberRelation.child,
-								FamilyMemberRelation.sibling,
-								FamilyMemberRelation.cousin,
-							] as FamilyMemberRelation[])
-						: ([
-								FamilyMemberRelation.spouse,
-								FamilyMemberRelation.partner,
-								FamilyMemberRelation.child,
-								FamilyMemberRelation.sibling,
-								FamilyMemberRelation.cousin,
-								FamilyMemberRelation.parent,
-								FamilyMemberRelation.parentInLaw,
-								FamilyMemberRelation.grandparent,
-								FamilyMemberRelation.grandparentInLaw,
-							] as FamilyMemberRelation[]),
-				);
-				break;
-			}
-			default:
-				break;
-		}
-		console.log(
-			'RelationshipFormComponent.setRelationships()',
-			this.$spaceRef(),
-			this.relationshipOptions,
-		);
-	}
-
-	protected onRelationshipChanged(event: Event): void {
-		event.stopPropagation();
-		const ce = event as CustomEvent;
-		const value = ce.detail.value as string;
+	protected onRelationshipChanged(value: string): void {
 		console.log('onRelationshipChanged()', value);
 		const created: IWithCreatedShort = {
 			at: new Date().toISOString().substring(0, 10),
