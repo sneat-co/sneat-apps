d7cdc33cd019c508714f4f55bfd61d47e3c73189
Alexander Trakhimenok <alex@trakhimenok.com>
2025-11-22 06:34:16 +0000

    fix: use const ph = posthog as unknown as PostHog; in posthog-analytics.service.ts

--- libs/core/src/lib/anaylytics/posthog-analytics.service.ts
+++ libs/core/src/lib/anaylytics/posthog-analytics.service.ts
@@ -1,14 +1,16 @@
-import { Injectable, inject } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ErrorLogger, IErrorLogger } from '@sneat/logging';
-import posthog from 'posthog-js';
+import posthog, { PostHog } from 'posthog-js';
 import { IAnalyticsService } from './analytics.interface';
+
+const ph = posthog as unknown as PostHog;
 
 @Injectable()
 export class PosthogAnalyticsService implements IAnalyticsService {
 	private errorLogger = inject<IErrorLogger>(ErrorLogger);
 
 	public identify(userID: string): void {
-		posthog.identify(userID);
+		ph.identify(userID);
 	}
 
 	public logEvent(
@@ -16,7 +18,7 @@ export class PosthogAnalyticsService implements IAnalyticsService {
 		eventParams?: Readonly<Record<string, unknown>>,
 	): void {
 		try {
-			posthog.capture(eventName, eventParams);
+			ph.capture(eventName, eventParams);
 		} catch (e) {
 			this.errorLogger.logError(e, 'Failed to log event to Posthog', {
 				show: false,
@@ -26,7 +28,7 @@ export class PosthogAnalyticsService implements IAnalyticsService {
 
 	public setCurrentScreen(screenName: string): void {
 		try {
-			posthog.capture('$screen_view', { screen_name: screenName });
+			ph.capture('$screen_view', { screen_name: screenName });
 		} catch (e) {
 			this.errorLogger.logError(e, 'Failed to log screen view to Posthog', {
 				show: false,
@@ -36,7 +38,7 @@ export class PosthogAnalyticsService implements IAnalyticsService {
 
 	public loggedOut() {
 		try {
-			posthog.reset();
+			ph.reset();
 		} catch (e) {
 			this.errorLogger.logError(e, 'Failed to reset Posthog', { show: false });
 		}
