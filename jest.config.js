fdc6250b4023ef50eaf434fa106b6110c9323b98
Alexander Trakhimenok <alex@trakhimenok.com>
2026-02-07 06:04:28 +0000

    fix: test configurations fixed

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// @ts-expect-error TODO: @nx/jest is not found
-import { getJestProjectsAsync } from '@nx/jest';
-export default async () => ({
-	projects: await getJestProjectsAsync(),
-});
-//# sourceMappingURL=jest.config.js.map
