02c368b09c6b105c470924c11a7949adf2ef6936
Alexander Trakhimenok <alex@trakhimenok.com>
2026-02-09 22:30:49 +0000

    fix(ci): run sneat-server in GH action

--- scripts/serve_backend.sh
+++ scripts/serve_backend.sh
@@ -3,6 +3,14 @@
 # Resolve directory where this script lives
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Call target script relative to this file
-# We make assumption you have access backed repositories and it's cloned
-"$SCRIPT_DIR/../../sneat-go-server/serve_gae.sh"
+
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  echo "Running in GitHub Actions"
+  ./sneat-server
+else
+  # Resolve directory where this script lives
+  SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+  # We assume backend repo is cloned
+  "$SCRIPT_DIR/../../sneat-go-server/serve_gae.sh"
+fi
