0f3cf865dae7b5a8ab2ab54490f7da59d9917ec7
Alexander Trakhimenok <alex@trakhimenok.com>
2026-02-10 09:05:12 +0000

    fix(ci): detect CI

--- scripts/serve_firebase_emulators.sh
+++ scripts/serve_firebase_emulators.sh
@@ -3,5 +3,10 @@
 # Resolve directory where this script lives
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-"$SCRIPT_DIR/../../sneat-firebase/scripts/serve_fb_emulators_ssl.sh"
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  echo "Running in GitHub Actions"
+# TODO: Clone https://github.com/sneat-co/sneat-firebase repository
+fi
 
+# We assume sneat-firebase repo is cloned
+"$SCRIPT_DIR/../../sneat-firebase/scripts/serve_fb_emulators_ssl.sh"
